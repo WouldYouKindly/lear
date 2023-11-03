d7ee91401b7a80ff6698f139908c5813494e7d5c
Alexey <forestbiiird@gmail.com>
2023-11-03 12:42:16 +0000

    Remove duplication between white-key and piano-octave

--- pure/piano-octave.js
+++ pure/piano-octave.js
@@ -5,6 +5,8 @@ const setup = async () => {
     const template = parser.parseFromString(html, 'text/html').querySelector('template')
   
     return class PianoOctave extends HTMLElement {
+        static observedAttributes = ["data-octave"];
+
         KEYS = ["C", 'C#', "D", 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
         constructor() { super() }
@@ -12,26 +14,33 @@ const setup = async () => {
         connectedCallback() {
             const shadowRoot = this.attachShadow({ mode: "open" });
             const clone = template.content.cloneNode(true);
-            const octave = this.attributes['data-octave'].nodeValue;
             const containerDiv = clone.children[1];
 
-            const keys = Array.from(containerDiv.querySelectorAll('.white, .black'));
+            const keys = Array.from(containerDiv.querySelectorAll('white-key, .black'));
             for (let [index, key] of keys.entries()) {
                 key.onmousedown = (evt) => {
                     this.dispatchEvent(new CustomEvent("pianokeydown", {
-                        detail: this.KEYS[index] + octave
+                        detail: this.KEYS[index] + this.octave
                     }));
                 }
                 key.onmouseup = (evt) => {
                     this.dispatchEvent(new CustomEvent("pianokeyup", {
-                        detail: this.KEYS[index] + octave
+                        detail: this.KEYS[index] + this.octave
                     }));
                 }
             }
             
-            // containerDiv.querySelector('#octave-name').textContent = "C" + octave;
+            containerDiv
+              .querySelector('white-key')
+              .setAttribute("data-note-hint", "C" + this.octave);
 
             shadowRoot.appendChild(clone);
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === "data-octave") {
+                this.octave = newValue;
+            }
         }
     }
   }
