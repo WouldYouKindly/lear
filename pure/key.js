0200c09e9f038d479704493cba5d5581805ef485
Alexey <forestbiiird@gmail.com>
2023-11-18 19:07:45 +0000

    Allow sliding from one note to another

--- pure/key.js
+++ pure/key.js
@@ -3,27 +3,20 @@ class Key extends HTMLElement {
 
     constructor() {
         super();
-
-        this.onmousedown = (evt) => {
-            if (!this.note) throw new Error('Note was not set!');
-
-            this.dispatchEvent(new CustomEvent("pianokeydown", {
+        const data = () => {
+            if (!this.note) throw new Error('Note is not set!');
+            return {
                 detail: this.note,
                 bubbles: true,
                 composed: true,
-            }));
-        }
-        this.onmouseup = (evt) => {
-            if (!this.note) throw new Error('Note was not set!');
-            
-            this.dispatchEvent(new CustomEvent("pianokeyup", {
-                detail: this.note,
-                bubbles: true,
-                composed: true,
-            }));
-        }
+            };
+        };
+
+        this.onmousedown = evt => this.dispatchEvent(new CustomEvent("pianokeydown", data()));
+        this.onmouseup = evt => this.dispatchEvent(new CustomEvent("pianokeyup", data()))
+        this.onmouseenter = evt => this.dispatchEvent(new CustomEvent("pianokeyenter", data()));
+        this.onmouseleave = evt => this.dispatchEvent(new CustomEvent("pianokeyleft", data()));
     }
 }
-
 
 export {Key};
